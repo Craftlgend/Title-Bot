e3b186778e44b2edf29f404e60acbdc3ae8cc606
Craftlgend <constantin.rasch@yahoo.com>
2024-04-26 22:08:31 +0200

    modified:   discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -1,8 +1,15 @@
-import discord 
-TOKEN = "MTIzMjc2MTkxNzYyNjM4ODcyMg.G7BEBt.k4fjN3--Ho04RI7h-SRHVhplnego3tpb6OFFTk"
-client = discord.Client(intents=discord.Intents.default())
+import os
+
+import discord
+
+TOKEN = ('MTIzMjc2MTkxNzYyNjM4ODcyMg.G7BEBt.k4fjN3--Ho04RI7h-SRHVhplnego3tpb6OFFTk')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
 @client.event
 async def on_ready():
-    print(f'{client.user} ist mit folgenden Servern verbunden:\n')
-    for server in client.guilds:
-        print(f'{server.name}(id: {server.id})')
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
