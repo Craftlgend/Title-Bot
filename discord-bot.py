4aaa327d42971503c2b5beecf2f5402e6d146b05
Craftlgend <constantin.rasch@yahoo.com>
2024-04-26 22:39:00 +0200

    modified:   discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -1,15 +1,26 @@
-import os
-
 import discord
+from discord.ext import commands
 
 TOKEN = ('MTIzMjc2MTkxNzYyNjM4ODcyMg.G7BEBt.k4fjN3--Ho04RI7h-SRHVhplnego3tpb6OFFTk')
+GUILD={'PRf0 Academy'}
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run(TOKEN)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+bot.run(TOKEN)
+
+
