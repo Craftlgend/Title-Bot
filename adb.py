16788142ba7af27c29bda2470557c26cd1703d30
Craftlgend <constantin.rasch@yahoo.com>
2024-04-27 20:25:37 +0200

    modified:   adb.py 	new file:   start.py

--- adb.py
+++ adb.py
@@ -1,11 +1,13 @@
 from ppadb.client import Client
-import numpy
+import numpy as np
 import time
 import subprocess
 import os
 
-subprocess.run('adb start-server', stdout=subprocess.PIPE, shell=True)
 
+subprocess.run('adb start-server', stdout=subprocess.PIPE, shell=True)
+x = 650
+y = 350
 
 client = Client(host='127.0.0.1' , port=5037)
 client.remote_connect('127.0.0.1', 5725)
@@ -15,6 +17,6 @@ devices = client.devices()
 device = devices[0]
 print (device)
 
-device.shell('input touchscreen swipe 200 400 500 600')
+device.shell(f'input touchscreen tap {int(x)} {int(y)}')
 time.sleep(10)
 subprocess.run("adb kill-server", stdout=subprocess.PIPE, shell=True)
